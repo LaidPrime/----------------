f5d819e17e155ec48a91267a048f03fa54b57f63
LaidPrime <egorpusher@gmail.com>
2025-01-08 13:21:46 +0500

    убрал нерабочию картинку

--- for_slider.js
+++ for_slider.js
@@ -1,9 +1,7 @@
 let sliders = document.querySelectorAll('.slider');
-// sliders - список всех элементов с классом class="slider"
-// sliders[0] — первый элемент, sliders[1] — второй, sliders[i] — i-тый.
 
 for (let i = 0; i < sliders.length; i++) {
-    // Перебирает все, вызывает функцию для каждого.
+    // Перебирает все, вызывает функцию для каждого слайдера.
     init_slider(sliders[i]);
 }
 
@@ -25,17 +23,14 @@ function init_slider(slider) {
 
     function showNextSlide() {
         slide[i].classList.remove('block');
-        // slide[i] - открытый слайд. Скрываем.
 
         i = (i + 1) % slide.length; // (*1)
 
         slide[i].classList.add('block');
-        // slide[i] - следующий слайд. Показываем.
     }
 
     function showPrevSlide() {
         slide[i].classList.remove('block');
-        // slide[i] - открытый слайд. Скрываем.
         if (i == 0) {
             i = slide.length - 1;
         }
@@ -44,48 +39,5 @@ function init_slider(slider) {
         }
 
         slide[i].classList.add('block');
-        // slide[i] - следующий слайд. Показываем.
     }
 }
-
-
-
-
-
-// Получаем элементы слайдера
-// let slider = document.querySelector('.slider');
-// let prevButton = document.querySelector('.prev-button');
-// let nextButton = document.querySelector('.next-button');
-// let slides = Array.from(slider.querySelectorAll('img'));
-// let slideCount = slides.length;
-// let slideIndex = 0;
-
-// // Устанавливаем обработчики событий для кнопок
-// prevButton.addEventListener('click', showPreviousSlide);
-// nextButton.addEventListener('click', showNextSlide);
-
-// // Функция для показа предыдущего слайда
-// function showPreviousSlide() {
-//     slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-//     updateSlider();
-// }
-
-// // Функция для показа следующего слайда
-// function showNextSlide() {
-//     slideIndex = (slideIndex + 1) % slideCount;
-//     updateSlider();
-// }
-
-// // Функция для обновления отображения слайдера
-// function updateSlider() {
-//     slides.forEach((slide, index) => {
-//         if (index === slideIndex) {
-//             slide.style.display = 'block';
-//         } else {
-//             slide.style.display = 'none';
-//         }
-//     });
-// }
-
-// Инициализация слайдера
-// updateSlider();
